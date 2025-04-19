1fc2de866f17281f970cefac7448a7f44c901c53
Samuel Sparenga <sxmuelspxr@gmail.com>
2025-04-19 11:39:22 -0700

    Arduino Steering Cleanup for Lab

--- serialSteering/serialSteering.ino
+++ serialSteering/serialSteering.ino
@@ -2,13 +2,6 @@
 
 Servo steeringServo;
 
-int steeringPos = 0;    // variable to store the servo position 
-int pinSTR      = 9;
- 
-String cmdRec = "";
-String cmd    = "";
-String value  = "";
-
 const int right_enable_a = 3;
 const int right_input_a  = 4;
 const int right_input_b  = 5;
@@ -16,132 +9,181 @@ const int right_input_b  = 5;
 const int left_enable_a = 6;
 const int left_input_a  = 7;
 const int left_input_b  = 8;
+const int pinSTR        = 9;
+ 
+String cmdRec = "";
+String cmd    = "";
+String value  = "";
 
 int portSpeed = 9600;
+int steeringPos = 0;    // variable to store the servo position
 
 void ack_turn (int spd, int theta)
 {
-  // Assuming W/L ratio ~ 1/3
-  double wl_const = 1 / ( 3 * tan ( theta * DEG_TO_RAD ) );
+   double wl_const = (16.25) / ( (23.50) * tan ( theta * DEG_TO_RAD ) );
   
-  double r_v = 0; spd * (1 - 0.5 * (wl_const));
-  double l_v = 0; spd * (1 + 0.5 * (wl_const));
+   double r_v = 0; spd * (1 - 0.5 * (wl_const));
+   double l_v = 0; spd * (1 + 0.5 * (wl_const));
   
-  if ( (theta * DEG_TO_RAD) < (PI / 2) )
-  {
-    r_v = spd * (1 + 0.5 * (wl_const));
-    l_v = spd * (1 - 0.5 * (wl_const));
+   if ( (theta * DEG_TO_RAD) < (PI / 2) )
+   {
+      r_v = spd * (1 + 0.5 * (wl_const));
+      l_v = spd * (1 - 0.5 * (wl_const));
     
-    Serial.print ("LEFT WHEEL V : ");
-  	Serial.println (l_v);
+      Serial.print ("LEFT WHEEL VELOCITY : ");
+      Serial.println (l_v);
   
-  	Serial.print ("RIGHT WHEEL V : ");
-  	Serial.println (r_v);
+      Serial.print ("RIGHT WHEEL VELOCITY : ");
+      Serial.println (r_v);
   
-  	digitalWrite (left_input_a, HIGH);
-  	digitalWrite (left_input_b, LOW);
-  	analogWrite (left_enable_a, r_v);
+      digitalWrite (left_input_a, HIGH);
+      digitalWrite (left_input_b, LOW);
+      analogWrite (left_enable_a, r_v);
   
-  	digitalWrite (right_input_a, HIGH);
-  	digitalWrite (right_input_b, LOW);
-  	analogWrite (right_enable_a, l_v);
+      digitalWrite (right_input_a, HIGH);
+      digitalWrite (right_input_b, LOW);
+      analogWrite (right_enable_a, l_v);
     
-  	delay (500);
-  }
-  else if ( (theta * DEG_TO_RAD) > (PI / 2) )
-  {
-    r_v = spd * (1 - 0.5 * (wl_const));
-    l_v = spd * (1 + 0.5 * (wl_const));
+      delay (500);
+   }
+   else if ( (theta * DEG_TO_RAD) > (PI / 2) )
+   {
+      r_v = spd * (1 - 0.5 * (wl_const));
+      l_v = spd * (1 + 0.5 * (wl_const));
     
-    Serial.print ("LEFT WHEEL V : ");
-  	Serial.println (l_v);
+      Serial.print ("LEFT WHEEL VELOCITY : ");
+      Serial.println (l_v);
   
-  	Serial.print ("RIGHT WHEEL V : ");
-  	Serial.println (r_v);
+      Serial.print ("RIGHT WHEEL VELOCITY : ");
+      Serial.println (r_v);
   
-  	digitalWrite (right_input_a, HIGH);
-  	digitalWrite (right_input_b, LOW);
-  	analogWrite (right_enable_a, r_v);
+      digitalWrite (right_input_a, HIGH);
+      digitalWrite (right_input_b, LOW);
+      analogWrite (right_enable_a, r_v);
   
-  	digitalWrite (left_input_a, HIGH);
-  	digitalWrite (left_input_b, LOW);
-  	analogWrite (left_enable_a, l_v);
+      digitalWrite (left_input_a, HIGH);
+      digitalWrite (left_input_b, LOW);
+      analogWrite (left_enable_a, l_v);
     
-  	delay (500);
-  }
+      delay (500);
+   }
+   else 
+   {
+      // -- Wheel Diameter 2.5625 inches
+      // -- Wheel Circumference 8.04625 inches | 0.2043 m
+      // -- 1 degree = 0.02235 inches
+
+      r_v = spd * (1 - 0.5 * (wl_const));
+      
+      Serial.print ("CURRENT VELOCITY : ");
+      Serial.println (r_v);
+
+      digitalWrite (right_input_a, HIGH);
+      digitalWrite (right_input_b, LOW);
+      analogWrite (right_enable_a, r_v);
+   
+      digitalWrite (left_input_a, HIGH);
+      digitalWrite (left_input_b, LOW);
+      analogWrite (left_enable_a, r_v);
+
+      delay (500);
+   }
 }
 
 void straighten ()
 {
-  steeringPos = 90;
-  steeringServo.write (steeringPos);
+   steeringPos = 90;
+   steeringServo.write (steeringPos);
   
-  digitalWrite (left_input_a, LOW);
-  digitalWrite (left_input_b, LOW);
-  analogWrite (left_enable_a, 0);
+   digitalWrite (left_input_a, LOW);
+   digitalWrite (left_input_b, LOW);
+   analogWrite (left_enable_a, 0);
   
-  digitalWrite (right_input_a, LOW);
-  digitalWrite (right_input_b, LOW);
-  analogWrite (right_enable_a, 0);
+   digitalWrite (right_input_a, LOW);
+   digitalWrite (right_input_b, LOW);
+   analogWrite (right_enable_a, 0);
   
-  cmd = "";
-  value = "";
+   cmd = "";
+   value = "";
 }
 
+
+void reverse (int spd)
+{
+   digitalWrite (left_input_a, LOW);
+   digitalWrite (left_input_b, HIGH);
+   analogWrite (left_enable_a, spd);
+   
+   digitalWrite (right_input_a, LOW);
+   digitalWrite (right_input_b, HIGH);
+   analogWrite (right_enable_a, spd);
+   
+   cmd = "";
+   value = "";
+} 
 
 
 void setup () 
 {  
-  // seria port setup
-  Serial.begin(portSpeed);
+   // serial port setup
+   Serial.begin(portSpeed);
   
-  pinMode(right_enable_a, OUTPUT);
-  pinMode(right_input_a, OUTPUT);
-  pinMode(right_input_b, OUTPUT);
+   pinMode(right_enable_a, OUTPUT);
+   pinMode(right_input_a, OUTPUT);
+   pinMode(right_input_b, OUTPUT);
   
-  pinMode(left_enable_a, OUTPUT);
-  pinMode(left_input_a, OUTPUT);
-  pinMode(left_input_b, OUTPUT);
+   pinMode(left_enable_a, OUTPUT);
+   pinMode(left_input_a, OUTPUT);
+   pinMode(left_input_b, OUTPUT);
     
-  // steering setup
-  pinMode(pinSTR, OUTPUT);
-  steeringServo.attach(pinSTR);  // attaches the servo on pin 9 to the servo object  
+   // steering setup
+   pinMode(pinSTR, OUTPUT);
+   steeringServo.attach(pinSTR);  // attaches the servo on pin 9 to the servo object  
 } // --- SETUP END ---
 
 void loop () 
 {
-  //read serial port
-  if ( Serial.available () ) 
-  {      
-    cmdRec = Serial.readStringUntil ('\n');
-    cmd    = cmdRec.substring (0,3);
-    value  = cmdRec.substring (3,6);
+   //read serial port
+   if ( Serial.available () ) 
+   {      
+      cmdRec = Serial.readStringUntil ('\n');
+      cmd    = cmdRec.substring (0,3);
+      value  = cmdRec.substring (3,6);
     
-    //Serial.print("Recieved ");
-    Serial.println (cmd);
-    //Serial.print("and ");
-    Serial.println (value);
-  }
-  
-  // -- Wheel Diameter 2.5625 inches
-  // -- Wheel Circumference 8.04625 inches
-  // -- 1 degree = 0.02235 inches
-  
-  if ( cmd =="STR" )
-  {
+      //Serial.print("Recieved ");
+      Serial.println (cmd);
+
+      //Serial.print("and ");
+      Serial.println (value);
+   }
+    
+   if ( cmd == "STR" )
+   {
       steeringPos = value.toInt ();
       steeringServo.write (steeringPos);
       delay (1000);
-  }
-  else if ( cmd == "STP" )
-  {
+   }
+   else if ( cmd == "STP" )
+   {
       straighten ();
       delay (1000);
-  }
-  else if ( cmd == "FWD")
-  {
-    ack_turn (value.toInt (), 45);
-    delay (1000);
-  }  
+   }
+   else if ( cmd == "FWD")
+   {
+      ack_turn (value.toInt (), 90);
+      delay (1000);
+   }
+   else if ( cmd == "REV" )
+   {
+      reverse (value.toInt ());
+      delay (1000);
+   }
+   // else if ( cmd == "" )
+   // {}
+   else
+   {
+      straighten ();
+      delay (1234);
+   }  
 } // --- LOOP END ---
 
